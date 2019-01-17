e5561a4249c835af6361daf308fd9fefdf352ad7
Johann Kuindji <kuindji@gmail.com>
2019-01-17 16:10:52 +0300

    small dependency fix

--- src/lib/Observable.js
+++ src/lib/Observable.js
@@ -1,6 +1,5 @@
 
-var bind = require("metaphorjs-shared/src/func/bind.js"),
-    extend = require("metaphorjs-shared/src/func/extend.js"),
+var extend = require("metaphorjs-shared/src/func/extend.js"),
     toArray = require("metaphorjs-shared/src/func/toArray.js"),
     MetaphorJs = require("metaphorjs-shared/src/MetaphorJs.js");
     
